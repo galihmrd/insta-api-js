c1d4d2ba73d2d7e209622522b030520b96201618
GalihMrd <69835093+galihmrd@users.noreply.github.com>
2024-08-24 04:29:29 +0700

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,8 @@
 const express = require('express');
 const instagramGetUrl = require("instagram-url-direct")
-
+const { ndown } = require("nayan-media-downloader")
 const app = express();
+
 app.use(express.static('public'));
 
 
@@ -10,9 +11,20 @@ async function instaRequest(url){
     return result
 }
 
+async function instaRequest2(url){
+    let result = await ndown(url)
+    return result
+}
+
 app.get('/instagram', async function (req, res) {
     const inputUrl = req.query.url
     const output = await instaRequest(inputUrl)
+    res.send(output);
+});
+
+app.get('/instagram2', async function (req, res) {
+    const inputUrl = req.query.url
+    const output = await instaRequest2(inputUrl)
     res.send(output);
 });
 
